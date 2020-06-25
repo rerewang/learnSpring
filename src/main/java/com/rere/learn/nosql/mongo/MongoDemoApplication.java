f1bbd2651e3957737ff98dce833e534a3489946b
rere.wang <rere22.wang@gmail.com>
2020-06-25 23:36:47 +0800

    MongoDemo first commit

---
+++ src/main/java/com/rere/learn/nosql/mongo/MongoDemoApplication.java
@@ -0,0 +1,26 @@
+package com.rere.learn.nosql.mongo;
+
+import com.rere.learn.nosql.mongo.model.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@SpringBootApplication
+@Slf4j
+public class MongoDemoApplication implements ApplicationRunner {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Coffee coffee = Coffee.builder().build();
+    }
+}
