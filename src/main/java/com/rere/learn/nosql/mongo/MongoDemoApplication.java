a0641d2b7caa0ac50a787fdc4b7169f5578b4da5
rere.wang <rere22.wang@gmail.com>
2020-06-26 23:57:12 +0800

    fix

--- src/main/java/com/rere/learn/nosql/mongo/MongoDemoApplication.java
+++ src/main/java/com/rere/learn/nosql/mongo/MongoDemoApplication.java
@@ -21,6 +21,5 @@ public class MongoDemoApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Coffee coffee = Coffee.builder().build();
     }
 }
