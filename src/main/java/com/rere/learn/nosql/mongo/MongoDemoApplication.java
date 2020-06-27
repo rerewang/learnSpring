4df6bfb2db56632bd0020f99fb8d441078f3a5a5
rere.wang <rere22.wang@gmail.com>
2020-06-27 23:47:17 +0800

    mongo converter

--- src/main/java/com/rere/learn/nosql/mongo/MongoDemoApplication.java
+++ src/main/java/com/rere/learn/nosql/mongo/MongoDemoApplication.java
@@ -1,13 +1,24 @@
 package com.rere.learn.nosql.mongo;
 
+import com.rere.learn.nosql.mongo.conventer.MoneyReadConverter;
 import com.rere.learn.nosql.mongo.model.Coffee;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 @Slf4j
@@ -19,7 +30,29 @@ public class MongoDemoApplication implements ApplicationRunner {
         SpringApplication.run(MongoDemoApplication.class, args);
     }
 
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(Arrays.asList(new MoneyReadConverter()));
+    }
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Coffee espresso = Coffee.builder()
+                .name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+        Coffee saved = mongoTemplate.save(espresso);
+        log.info("Coffee :{}", saved);
+
+        List<Coffee> list = mongoTemplate.find(
+                Query.query(Criteria.where("name").is("espresso")), Coffee.class
+        );
+        log.info("Find {} Coffee", list.size());
+        list.forEach(c -> log.info("Coffee {}", c));
+
+        //Thread.sleep(1000);
+
     }
 }
